a5ada9d7cb2635534771ffc696f7cfed60c46aa1
HANIWalid76 <122229831+HANIWalid76@users.noreply.github.com>
2023-01-08 20:38:03 +0100

    SU

--- set_env.sh
+++ set_env.sh
@@ -2,11 +2,11 @@
 
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=admin
-export POSTGRES_PASSWORD=admin1234
-export POSTGRES_HOST=udagramd-db
-export POSTGRES_DB=udagramdb
-export AWS_BUCKET=udagram-esiea-8765
+export POSTGRES_USERNAME=HANIUSE
+export POSTGRES_PASSWORD=HANI
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=HANIDB
+export AWS_BUCKET=udagram-esiea-hani
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
